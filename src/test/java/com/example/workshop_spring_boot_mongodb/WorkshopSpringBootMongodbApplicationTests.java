83227884027db87ff51c9a4f1548fd239395ef2c
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-09-01 18:32:02 -0300

    first commit

---
+++ src/test/java/com/example/workshop_spring_boot_mongodb/WorkshopSpringBootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workshop_spring_boot_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringBootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
